0030b13702577b1f3d9f38e48b4f3bfffb1c56f8
albertofcyber98 <yohanesalbert2@gmail.com>
2022-01-19 00:16:38 +0800

    Tugas Redux Counter

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import TugasPortalBerita from './TugasPortalBerita/Index';
 // import TugasFunction from './TugasFunction/Index';
 // import TugasFormValidation from './TugasFormValidation/Index';
-import Routing from './TugasRouting';
+// import Routing from './TugasRouting';
+import Redux from './TugasRedux';
 
 
 function App() {
   return (
     <div>
-      <Routing/>
+      <Redux/>
     </div>
   );
 }
---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import counterReducer from "./features/Counter/reducer";
+import thunk from 'redux-thunk';
+
+let rootReducers = combineReducers({
+    counter: counterReducer
+})
+const composeEnhancers = window.__REDUX_DEVTOOLS_ENTENSION_COMPOSE__ || compose;
+let store = createStore(rootReducers, composeEnhancers(applyMiddleware(thunk)));
+export default store;
